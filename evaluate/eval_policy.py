ea1099cbe4b1c73ef8837c034bae3558bbb27aaa
ting-yang14 <sheep870104@gmail.com>
2025-05-13 16:20:44 +0800

    fix: correctly calculate overall reward during model testing

--- evaluate/eval_policy.py
+++ evaluate/eval_policy.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 def evaluate_agent(env, agent):
     """評估 DQN 代理"""
     state, _ = env.reset()
-    reward = 0
+    overall_reward = 0
     done = False
     truncated = False
     ground_truth = []
@@ -15,7 +15,7 @@ def evaluate_agent(env, agent):
         action = agent.get_action(state, eval_mode=True)
         next_state, reward, done, truncated, info = env.step(action)
         state = next_state
-        reward += reward
+        overall_reward += reward
         ground_truth.append(info["ground_truth"])
         predictions.append(info["prediction"])
 
@@ -25,7 +25,7 @@ def evaluate_agent(env, agent):
     recall = recall_score(ground_truth, predictions, zero_division=0)
 
     return (
-        reward,
+        overall_reward,
         accuracy,
         precision,
         recall,
